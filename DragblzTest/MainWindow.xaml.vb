eccf4fc362aa79109a5b4565cb6b39615306452f
Lance Boudreaux <me_lance@msn.com>
2015-08-22 10:12:53 -0500

    Initial

---
+++ DragblzTest/MainWindow.xaml.vb
@@ -0,0 +1,18 @@
+﻿Class MainWindow 
+
+    Public Shared ReadOnly InterTabClientProperty As DependencyProperty = DependencyProperty.Register("InterTabClientInstance",
+                                                                                                      GetType(MyInterTabClient),
+                                                                                                      GetType(MainWindow),
+                                                                                                      New PropertyMetadata(Nothing))
+
+    Public Property InterTabClientInstance As MyInterTabClient
+        Get
+            If GetValue(InterTabClientProperty) Is Nothing Then InterTabClientInstance = New MyInterTabClient
+            Return DirectCast(GetValue(InterTabClientProperty), MyInterTabClient)
+        End Get
+        Set(value As MyInterTabClient)
+            SetValue(InterTabClientProperty, value)
+        End Set
+    End Property
+
+End Class
---
+++ DragblzTest/MyInterTabClient.vb
@@ -0,0 +1,15 @@
+﻿Public Class MyInterTabClient
+    Implements Dragablz.IInterTabClient
+
+    Public Function GetNewHost(interTabClient As Dragablz.IInterTabClient, partition As Object, source As Dragablz.TabablzControl) As Dragablz.INewTabHost(Of Window) Implements Dragablz.IInterTabClient.GetNewHost
+        Dim host As New TabHost
+
+        host.tabMain.InterTabController = New Dragablz.InterTabController With {.InterTabClient = Me}
+
+        Return New Dragablz.NewTabHost(Of TabHost)(host, host.tabMain)
+    End Function
+
+    Public Function TabEmptiedHandler(tabControl As Dragablz.TabablzControl, window As Window) As Dragablz.TabEmptiedResponse Implements Dragablz.IInterTabClient.TabEmptiedHandler
+        Return Dragablz.TabEmptiedResponse.CloseWindowOrLayoutBranch
+    End Function
+End Class
